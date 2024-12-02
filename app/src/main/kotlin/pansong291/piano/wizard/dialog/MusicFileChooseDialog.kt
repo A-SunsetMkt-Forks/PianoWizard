c163e02bd7e33f7cda664b5baf610e3414dd6e5f
pansong291 <pansong291@sina.com>
2024-12-02 11:19:26 +0800

    自动解压乐谱文件

--- app/src/main/kotlin/pansong291/piano/wizard/dialog/MusicFileChooseDialog.kt
+++ app/src/main/kotlin/pansong291/piano/wizard/dialog/MusicFileChooseDialog.kt
@@ -2,9 +2,10 @@ package pansong291.piano.wizard.dialog
 
 import android.annotation.SuppressLint
 import android.content.Context
-import com.hjq.toast.Toaster
+import android.view.View
 import pansong291.piano.wizard.R
 import pansong291.piano.wizard.consts.StringConst
+import java.io.File
 import java.io.FileFilter
 
 @SuppressLint("NotifyDataSetChanged")
@@ -16,17 +17,11 @@ class MusicFileChooseDialog(context: Context) : FileChooseDialog(context) {
             //  初始时未选择歌曲, 自动切歌, 支持循环和随机
             //  暂停时可切歌, 支持进度调整 (需要换算播放进度)
             //  在开始列表播放模式后，一次性读取列表缓存起来，切歌的时候不必刷新列表
-            ok.setText(R.string.make_as_default_folder)
-            ok.setOnClickListener {
-                sharedPreferences.edit()
-                    .putString(StringConst.SP_DATA_KEY_DEFAULT_FOLDER, adapter.basePath).apply()
-                Toaster.show(R.string.make_default_folder_feedback_message)
-            }
+            ok.visibility = View.GONE
         }
         setIcon(R.drawable.outline_music_file_32)
         setTitle(R.string.select_music)
-        sharedPreferences.getString(StringConst.SP_DATA_KEY_DEFAULT_FOLDER, null)
-            ?.let { adapter.basePath = it }
+        adapter.basePath = File(context.getExternalFilesDir(null), "yp").path
         adapter.fileFilter = FileFilter {
             it.isDirectory || it.name.endsWith(StringConst.MUSIC_NOTATION_FILE_EXT, true)
         }
